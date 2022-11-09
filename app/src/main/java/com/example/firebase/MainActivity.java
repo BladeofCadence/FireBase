1e0249b2604a0197e1d9fc7190b7643620283736
BladeofCadence <BladeofCadence@gmail.com>
2022-11-08 19:15:06 -0600

    basic

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseUser currentUser;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    // Check if user is signed in (non-null) and update UI accordingly.
+//    currentUser = mAuth.getCurrentUser();
+//
+//
+//    if (currentUser == null)
+//    {
+//        // nobody is logged in... we should probably show some
+//        //  buttons for "Login" and "Register" that will call our cool new Activities
+//    }
+//    else
+//    {
+//        // we have a user already logged in... cool.  What do we do with that?
+//    }
+
+//sign out
+//FirebaseAuth.getInstance().signOut();
+
 }
